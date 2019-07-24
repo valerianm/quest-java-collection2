d57322105e111968557dcb97e9a3080f5245d16f
valerian.m <valerian24@hotmail.fr>
2019-07-24 10:01:08 +0200

    Finished

--- Hero.java
+++ Hero.java
@@ -1,4 +1,6 @@
-public class Hero {
+import java.util.Comparator;
+
+public class Hero implements Comparable<Hero> {
 
     // attributs
     private String name;
@@ -27,4 +29,11 @@ public class Hero {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public int compareTo(Hero o) {
+        return this.getName().compareTo(o.getName());
+    }
+
+
 }
--- Thanos.java
+++ Thanos.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
@@ -13,20 +15,28 @@ public class Thanos {
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
 
-
+        Collections.sort(heroes);
         System.out.println("\nOrder by name:");
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
 
+        Comparator<Hero> ageCompare = new Comparator<Hero>() {
 
+            @Override
+            public int compare(Hero hero1, Hero hero2) {
+                return hero2.getAge() - hero1.getAge();
+            }
+        };
+
+        Collections.sort(heroes, ageCompare);
         System.out.println("\nOrder by age:");
         showList(heroes);
     }
 
     private static void showList(List<Hero> heroes) {
-        for (Hero heroe : heroes) {
-            System.out.println(heroe.getName() + ", " + heroe.getAge());
+        for (Hero hero : heroes) {
+            System.out.println(hero.getName() + ", " + hero.getAge());
         }
     }
 }
